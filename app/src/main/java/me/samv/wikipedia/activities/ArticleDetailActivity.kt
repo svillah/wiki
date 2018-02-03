1ac1306c71099dfdf3c5b8c4fc83ced030208075
Sam Villaluz <villaluzsam@gmail.com>
2018-02-03 16:46:24 -0500

    add search activity

--- app/src/main/java/me/samv/wikipedia/activities/ArticleDetailActivity.kt
+++ app/src/main/java/me/samv/wikipedia/activities/ArticleDetailActivity.kt
@@ -2,6 +2,7 @@ package me.samv.wikipedia.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import me.samv.wikipedia.R
 
 /**
@@ -12,5 +13,17 @@ class ArticleDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_detail)
+
+        setSupportActionBar(toolbar);
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true); //sets back button on LHS next to title
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean { //handles when back button is hit
+        if(item!!.itemId == android.R.id.home){
+            finish()
+        }
+        return true;
+    }
+
+
 }
