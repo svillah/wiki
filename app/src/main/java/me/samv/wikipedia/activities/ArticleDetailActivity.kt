9e2fe10429ea13d4621e3788eaebaa2e1f26efd3
Sam Villaluz <villaluzsam@gmail.com>
2018-02-03 15:46:07 -0500

    add main and article activity

---
+++ app/src/main/java/me/samv/wikipedia/activities/ArticleDetailActivity.kt
@@ -0,0 +1,16 @@
+package me.samv.wikipedia.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.samv.wikipedia.R
+
+/**
+ * Created by svillaluz on 2018-02-03.
+ */
+class ArticleDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}
