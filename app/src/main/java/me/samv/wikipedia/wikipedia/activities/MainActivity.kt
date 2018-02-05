1b3e789add38cf0809929fce0badc8adae075051
Sam Villaluz <villaluzsam@gmail.com>
2018-02-04 22:55:22 -0500

    add fragment switch and search iconz

--- app/src/main/java/me/samv/wikipedia/wikipedia/activities/MainActivity.kt
+++ app/src/main/java/me/samv/wikipedia/wikipedia/activities/MainActivity.kt
@@ -6,35 +6,50 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import me.samv.wikipedia.R
+import me.samv.wikipedia.wikipedia.fragments.ExplorerFragment
+import me.samv.wikipedia.wikipedia.fragments.FavouritesFragment
+import me.samv.wikipedia.wikipedia.fragments.HistoryFragment
 
 class
 MainActivity : AppCompatActivity() {
 
+//    instances of fragments as properties on activity
+    private val exploreFragment: ExplorerFragment
+    private val favouritesFragment: FavouritesFragment
+    private val historyFragment: HistoryFragment
+
+//    base constructor initialization
+    init {
+        exploreFragment = ExplorerFragment()
+        favouritesFragment = FavouritesFragment()
+        historyFragment = HistoryFragment()
+    }
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out) //fun animations when switching fragments yeehaw
+
+        when(item.itemId){ //Kotlin when = java switch, change container with appropriate fragment
+            R.id.navigation_explore -> transaction.replace(R.id.fragment_container, exploreFragment)
+            R.id.navigation_favourites -> transaction.replace(R.id.fragment_container, favouritesFragment)
+            R.id.navigation_history -> transaction.replace(R.id.fragment_container, historyFragment)
         }
-        false
+
+        transaction.commit()
+
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        message.setOnClickListener { view ->
-            startActivity(Intent(this, ArticleDetailActivity::class.java))
-        }
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, exploreFragment)
+        transaction.commit()
     }
 }
